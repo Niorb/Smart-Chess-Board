958be8cc0a104e37395a9208686f0556f16abf50
Robin DL <robin.delissnyder@gmail.com>
2026-08-26 23:54:52 +0200

    fix(frontend): update lastMoveUci with pending opponent move

--- Raspberry/frontend/src/components/AnalysisTab.tsx
+++ Raspberry/frontend/src/components/AnalysisTab.tsx
@@ -2164,13 +2164,7 @@ const AnalysisTab: React.FC<AnalysisTabProps> = ({ boardState }) => {
                         fen={analysis?.fen || eg.drill.fen}
                         legalMoves={analysis?.legal_moves ?? []}
                         inCheck={!!analysis?.in_check}
-                        lastMoveUci={
-                          eg.pending_reply
-                            ? `${eg.pending_reply.from_sq}${eg.pending_reply.to_sq}`
-                            : boardState.last_move
-                            ? `${String.fromCharCode(97 + boardState.last_move[0][0])}${boardState.last_move[0][1] + 1}${String.fromCharCode(97 + boardState.last_move[1][0])}${boardState.last_move[1][1] + 1}`
-                            : undefined
-                        }
+                        lastMoveUci={eg.pending_reply?.uci || boardState.physical?.pending_opponent_move?.uci || undefined}
                         myColor={eg.drill.player_color}
                         onMovePlayed={(uci: string) => { void sendAnalysisMove(uci); }}
                       />
